d7c81d3c6798bb1fe6d2185d49d957490840bd56
jzProg <john_z94@hotmail.com>
2021-05-12 03:00:41 +0300

    style fixes for ios

--- helpers/cssComponents.js
+++ helpers/cssComponents.js
@@ -11,25 +11,24 @@ export const ButtonContainer=styled.TouchableOpacity`
 export const Label = styled.Text`
     color: #fff;
     font-size: 18px;
-    font-weight: 700px;
-    padding: 20px; 
-    justify-self: center;
+    font-weight: bold;
+    padding: 20px;
 `
 
 export const BlackLabel = styled.Text`
     color: black;
     font-size: 18px;
     font-weight: bold;
-    padding: 20px; 
+    padding: 20px;
     align-self: center;
 `
 export const Filler = styled.View`
 padding:20px;
 `
 export const Input = styled.TextInput`
-height: 50px; 
-font-size: 20px; 
+height: 50px;
+font-size: 20px;
 width:400px;
 background-color: #fff;
-align-self: center; 
+align-self: center;
 `
